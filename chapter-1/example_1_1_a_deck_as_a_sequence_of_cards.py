e39f9b7e19e2dee2803de0d60c1e410fecbe38b9
Ruben Colomina <rcolomina@gmail.com>
2025-02-02 00:38:20 +0000

    init veresion

---
+++ chapter-1/example_1_1_a_deck_as_a_sequence_of_cards.py
@@ -0,0 +1,58 @@
+import collections
+
+Card =collections.namedtuple('Card', ['rank','suit'])
+
+class FrenchDeck:
+    ranks = [str(n) for n in range(2,11)] + list('JQKA')
+    suits = 'spades diamons clubs hearts'.split()
+
+    def __init__(self):
+        self._cards = [Card(rank, suit) for suit in self.suits for rank in self.ranks]
+
+    def __len__(self):
+        return len(self._cards)
+
+    def __getitem__(self, position):
+        return self._cards[position]
+
+
+
+if __name__ == "__main__":
+
+    deck = FrenchDeck()
+    print("\nusage of dunder method on deck to get length")
+    print(len(deck))
+
+    print("\naccess to check by __getitem__ e.g. deck[0],deck[12]")
+    print(deck[0],deck[12])
+
+    from random import choice
+
+    print("\nchoice a random card from deck")
+    print(choice(deck))
+
+    print("\ninterate over deck")
+    for card in deck: # doctest: +ELLIPSIS
+        print(card)
+
+    print("\ndeck in reversed order")
+    for card in reversed(deck): # doctest: +ELLIPSIS        
+        print(card)
+
+    is_in_deck = Card('Q', "hearts") in deck
+    print("\nis_in deck?",is_in_deck)
+
+
+    is_in_deck = Card('Q', "beasts") in deck in deck
+    print("\nis_in_deck?",is_in_deck)
+
+
+    suit_values = dict(spades=3, hearts=2, diamons=1, clubs=0)
+    def spades_high(card):
+        rank_value = FrenchDeck.ranks.index(card.rank)
+        return rank_value * len(suit_values) + suit_values[card.suit]
+
+    print("\nSorted deck")
+    for card in sorted(deck, key=spades_high):
+        print(card)
+    
